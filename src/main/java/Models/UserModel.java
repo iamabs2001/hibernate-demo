1e9ba1d5b3f51d4327d763a3daafa6f1b7aa7932
iamabs2001 <abhijeetsharma248@gmail.com>
2020-07-24 08:30:45 +0530

    Add records to table

--- src/main/java/Models/UserModel.java
+++ src/main/java/Models/UserModel.java
@@ -1,6 +1,5 @@
 package Models;
 
-import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
--- src/main/java/com/sigma/corejava/Programme.java
+++ src/main/java/com/sigma/corejava/Programme.java
@@ -4,12 +4,19 @@ import Models.UserModel;
 import com.sigma.corejava.configs.HibernateConfig;
 import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class Programme {
     
     public static void main(String args[]) {
       
         Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction t = session.beginTransaction();
+        
+//        UserModel thenewuser = new UserModel(6, "hero","123");
+//        session.save(thenewuser);
+//        t.commit();
+        
         
         List<UserModel> UsersList = session.createQuery("from UserModel").list();
         
